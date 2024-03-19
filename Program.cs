c048ef905ee9dc948691a1a958922f0f7b8c2e61
Coutouzis Kilian <125976109+Epouvaantail@users.noreply.github.com>
2024-03-19 09:12:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Tree {
+
+    static void Main()
+    {
+        int level = 1;
+        string path = @"C:/"; // Beware of your path
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        listerDossier(directoryInfo, level);
+    }
+
+    static void listerDossier(DirectoryInfo directoryInfo, int level)
+    {
+        foreach (var file in directoryInfo.GetFiles()) {
+            if(file.Attributes.HasFlag(FileAttributes.Hidden)) {
+                continue;
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(string.Concat(Enumerable.Repeat('.',level+1)) + file.Name);
+        }
+
+        foreach(var directory in directoryInfo.GetDirectories()) {
+            if(directory.Attributes.HasFlag(FileAttributes.Hidden)) {
+                continue;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(string.Concat(Enumerable.Repeat('.',level))+directory.Name);
+            listerDossier(directory, level+1);
+        }
+    }
+}
